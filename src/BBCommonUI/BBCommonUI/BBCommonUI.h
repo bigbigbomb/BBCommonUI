b9f757b8b8568ec855e783485d43b6ffd2af20c0
Brian Romanko <brian@bigbigbomb.com>
2013-02-08 10:24:59 -0600

    Starting project structure

---
+++ src/BBCommonUI/BBCommonUI/BBCommonUI.h
@@ -0,0 +1,13 @@
+//
+//  BBCommonUI.h
+//  BBCommonUI
+//
+//  Created by Brian Romanko on 2/8/13.
+//  Copyright (c) 2013 bigbigbomb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BBCommonUI : NSObject
+
+@end
