cdf84c8e46ec76c7a096e3c467119ff336ba9c18
Lee Brenner <lee@bigbigbomb.com>
2013-02-13 00:02:59 -0600

    -proper ui layout update when an item is removed

--- src/BBCommonUI/Views/BBWrapView.h
+++ src/BBCommonUI/Views/BBWrapView.h
@@ -9,6 +9,7 @@
 @private
     float _itemPadding;
     float _lineHeight;
+    float _availableRightSpace;
     CGPoint _currentPosition;
     UIEdgeInsets _edgeInsets;
     
@@ -16,6 +17,7 @@
 
 @property(nonatomic) float itemPadding;
 @property(nonatomic) float lineHeight;
+@property(nonatomic) float availableRightSpace;
 @property(nonatomic) CGPoint currentPosition;
 @property(nonatomic) UIEdgeInsets edgeInsets;
 
--- src/BBCommonUI/Views/BBWrapView.m
+++ src/BBCommonUI/Views/BBWrapView.m
@@ -4,18 +4,26 @@
 //
 #import "BBWrapView.h"
 
+@interface BBWrapView ()
+@property(nonatomic, strong) UIView *viewToRemove;
+
+@end
+
 @implementation BBWrapView
 
 @synthesize itemPadding = _itemPadding;
 @synthesize lineHeight = _lineHeight;
 @synthesize currentPosition = _currentPosition;
 @synthesize edgeInsets = _edgeInsets;
+@synthesize viewToRemove = _viewToRemove;
+
 
 - (id)initWithLineHeight:(float)lineHeight itemPadding:(float)itemPadding edgeInsets:(UIEdgeInsets)edgeInsets {
     self = [super init];
     if (self) {
         _lineHeight = lineHeight;
         _itemPadding = itemPadding;
+        _availableRightSpace = 0;
         _edgeInsets = edgeInsets;
         _currentPosition = CGPointMake(edgeInsets.left, edgeInsets.top);
     }
@@ -23,38 +31,19 @@
     return self;
 }
 
-//- (void)addSubview:(UIView *)view {
-//    CGPoint nextPosition = CGPointMake(self.currentPosition.x + view.frame.size.width, self.currentPosition.y);
-//    if (nextPosition.x > self.frame.size.width - self.edgeInsets.right)
-//    {
-//        self.currentPosition = CGPointMake(self.edgeInsets.left, self.currentPosition.y + self.lineHeight);
-//    }
-//    view.frame = CGRectMake(self.currentPosition.x, self.currentPosition.y, view.frame.size.width, view.frame.size.height);
-//    self.currentPosition = CGPointMake(self.currentPosition.x + view.frame.size.width + self.itemPadding, self.currentPosition.y);
-//    self.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, self.frame.size.width, self.currentPosition.y + self.lineHeight + self.edgeInsets.bottom);
-//    [super addSubview:view];
-//}
-//
-//- (void)insertSubview:(UIView *)view atIndex:(NSInteger)index {
-//    CGPoint nextPosition = CGPointMake(self.currentPosition.x + view.frame.size.width, self.currentPosition.y);
-//        if (nextPosition.x > self.frame.size.width - self.edgeInsets.right)
-//        {
-//            self.currentPosition = CGPointMake(self.edgeInsets.left, self.currentPosition.y + self.lineHeight);
-//        }
-//        view.frame = CGRectMake(self.currentPosition.x, self.currentPosition.y, view.frame.size.width, view.frame.size.height);
-//        self.currentPosition = CGPointMake(self.currentPosition.x + view.frame.size.width + self.itemPadding, self.currentPosition.y);
-//        self.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, self.frame.size.width, self.currentPosition.y + self.lineHeight + self.edgeInsets.bottom);
-//    [super insertSubview:view atIndex:index];
-//}
-
 - (void)layoutSubviewsCore {
+    self.currentPosition = CGPointMake(self.edgeInsets.left, self.edgeInsets.top);
     for (UIView *view in [self subviews]) {
+        if (self.viewToRemove == view)
+            continue;
         if (self.currentPosition.x + view.frame.size.width > self.frame.size.width + self.edgeInsets.right)
             self.currentPosition = CGPointMake(self.edgeInsets.left, self.currentPosition.y + self.lineHeight);
         view.frame = CGRectMake(self.currentPosition.x, self.currentPosition.y, view.frame.size.width, view.frame.size.height);
         self.currentPosition = CGPointMake(self.currentPosition.x + view.frame.size.width + self.itemPadding, self.currentPosition.y);
         self.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, self.frame.size.width, self.currentPosition.y + self.lineHeight + self.edgeInsets.bottom);
+        self.availableRightSpace = self.frame.size.width - self.currentPosition.x - self.edgeInsets.right;
     }
+    self.viewToRemove = nil;
 }
 
 - (void)didAddSubview:(UIView *)subview {
@@ -62,6 +51,7 @@
 }
 
 - (void)willRemoveSubview:(UIView *)subview {
+    self.viewToRemove = subview;
     [self layoutSubviewsCore];
 }
 
